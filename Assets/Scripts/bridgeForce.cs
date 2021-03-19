47d4edcfaca618eaac42c6cbdc70fc57ba567e6e
atemcozz <atemcozzartem@gmail.com>
2021-03-19 21:55:32 +0300

    Добавлены звуки веревочных мостов

--- Assets/Scripts/bridgeForce.cs
+++ Assets/Scripts/bridgeForce.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class bridgeForce : MonoBehaviour
 {
     HingeJoint2D joint;
+    AudioSource src;
+    AudioClip rip;
+    AudioClip bridge;
     // Start is called before the first frame update
     void Start()
     {
+        src = gameObject.AddComponent<AudioSource>();
+        src.playOnAwake = false;
+        src.loop = false;
         joint = GetComponent<HingeJoint2D>();
+        rip = Resources.Load<AudioClip>("Sounds/rip");
+        bridge = Resources.Load<AudioClip>("Sounds/bridge");
     }
 
     // Update is called once per frame
@@ -16,15 +24,24 @@ public class bridgeForce : MonoBehaviour
     {
         
     }
+    private void OnJointBreak2D(Joint2D joint)
+    {
+        src.PlayOneShot(rip);
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if((other.gameObject.GetComponent<Rigidbody2D>() != null) && (other.gameObject.GetComponent<Rigidbody2D>().mass >= 5) && (joint != null)) 
         { 
             
-        
+      
         
             joint.breakForce = 50f;
+            
         
        }
+        if ((other.gameObject.GetComponent<Rigidbody2D>() != null) && (other.gameObject.GetComponent<Rigidbody2D>().mass >= 1))
+        {
+            src.PlayOneShot(bridge);
+        }
     }
 }
