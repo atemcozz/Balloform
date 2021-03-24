4720fa8be2b7a10e8fc87e7ee6b3a06f6ecb59a2
atemcozz <atemcozzartem@gmail.com>
2021-03-24 18:18:01 +0300

    Добавлены эффекты на пули

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public float speed = 10;
-  
+    public int type = 0;
+    GameObject particle;
+    GameObject particle_speedy;
     [Header("Bullet lifetime in seconds")]
    public float lifeTime = 5f;
     // Start is called before the first frame update
     void Start()
     {
+        particle = Resources.Load<GameObject>("particle_bullet");
+        particle_speedy = Resources.Load<GameObject>("particle_bullet_speedy");
         StartCoroutine(destroyBullet());
         
     }
     public void OnCollisionEnter2D(Collision2D col)
-    { 
-       if(col.gameObject.layer != 11) Destroy(gameObject);
-
+    {
+        if (col.gameObject.layer != 11)
+        {
+            if (type == 0) Instantiate(particle, transform.position, transform.rotation);
+            else Instantiate(particle_speedy, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
 
     }
     // Update is called once per frame
